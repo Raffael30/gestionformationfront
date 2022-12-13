56e2536721a20711baa42a1503c06f5af1f1d787
reychenne <raphael.eychenne@live.fr>
2022-12-13 15:47:05 +0100

    Ajout de la sécurité et formulaires

--- src/app/components/utilisateur/liste-utilisateur/liste-utilisateur.component.ts
+++ src/app/components/utilisateur/liste-utilisateur/liste-utilisateur.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Region } from 'src/app/models/region';
+import { Role } from 'src/app/models/role';
+import { Utilisateur } from 'src/app/models/utilisateur';
+import { RoleService } from 'src/app/services/role.service';
+import { UtilisateurService } from 'src/app/services/utilisateur.service';
 
 @Component({
   selector: 'app-liste-utilisateur',
@@ -6,5 +11,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./liste-utilisateur.component.scss']
 })
 export class ListeUtilisateurComponent {
+
+  utilisateurs!:Utilisateur[];
+  utilisateur!:Utilisateur;
+  roles!:Role[];
+  role!:Role;
+  idRole!:number;
+  region!:Region;
+
+  constructor(private utilisateurService:UtilisateurService, private roleService:RoleService)
+  {}
+
+  ngOnInit(): void {
+    
+   this.utilisateur= new Utilisateur();
+    this.getAll();
+  }
+
+  getAll()
+  {
+    this.utilisateurService.getAll().subscribe(
+      response=>this.utilisateurs=response
+    )
+  }
+
+
+  ajouter()
+  {
+    console.log('ajouter')
+
+        this.utilisateurService.ajouter(this.utilisateur).subscribe(
+          response=>
+          {
+            console.log('ok')
+            this.getAll();
+          },
+          error=>
+          {
+            console.log(' non ok')
+            console.log(error.message)
+          }
+        )
+
+  }
+
+  supprimer(id:number)
+  {
+    this.utilisateurService.supprimer(id).subscribe(
+      response=>
+      {
+        this.getAll();
+      },
+      error=>
+      {
+        console.log(error.message)
+      }
+    )
+  }
+
+  modifier(id:number)
+  {
+    this.utilisateurService.getById(id).subscribe(
+      response=>
+      {
+       this.utilisateur=response;
+      },
+      error=>
+      {
+        console.log(error.message)
+      }
+    )
+  }
 
 }
--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class InterceptorService {
-
-  constructor() { }
+@Injectable({ providedIn: 'root' })
+export class InterceptorService implements HttpInterceptor {
+  intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if ( sessionStorage.getItem('token')) {
+        let auth=sessionStorage.getItem('token')?? "";
+        httpRequest=httpRequest.clone({
+            setHeaders:{
+                Authorization: auth
+            }
+          })
+         
+       
+  
+    }
+    return next.handle(httpRequest);
+  
+  }
 }
