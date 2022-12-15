229d82b5ee8eb0915c6724fd1cfef4b501c45dcc
reychenne <raphael.eychenne@live.fr>
2022-12-15 17:17:02 +0100

    correction liste prospects et routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { AuthenticateGuard } from './authenticate.guard';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { GestionFormationComponent } from './components/formation/gestion-formation/gestion-formation.component';
+import { GestionMessageComponent } from './components/message/gestion-message/gestion-message.component';
 import { AjoutProspectComponent } from './components/prospect/ajout-prospect/ajout-prospect.component';
-
 import { GestionProspectComponent } from './components/prospect/gestion-prospect/gestion-prospect.component';
 import { GestionRendezvousComponent } from './components/rendezvous/gestion-rendezvous/gestion-rendezvous.component';
 import { GestionUtilisateurComponent } from './components/utilisateur/gestion-utilisateur/gestion-utilisateur.component';
@@ -17,7 +17,7 @@ const routes : Route [] = [
   {path: 'utilisateurs/:nomRole', component: GestionUtilisateurComponent, canActivate: [AuthenticateGuard]},
   {path: 'rendezvous', component: GestionRendezvousComponent, canActivate: [AuthenticateGuard]},
   {path: 'formations', component: GestionFormationComponent, canActivate:[AuthenticateGuard]},
-  {path: 'messages', component:, canActivate:[AuthenticateGuard]},
+  {path: 'messages', component: GestionMessageComponent, canActivate:[AuthenticateGuard]},
   {path: 'prospects', component: GestionProspectComponent},
   {path: 'ajoutProspect', component: AjoutProspectComponent}
 
